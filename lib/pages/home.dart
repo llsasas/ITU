74fc0dc7e26898239bd4c568a0e245c63ad513eb
Jan Hranička <hranickajan@seznam.cz>
2023-11-05 12:54:50 +0100

    wrap scaffold in materialApp

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ituapp/pages/spotDescription.dart';
 
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
@@ -11,43 +11,41 @@ class HomeScreen extends StatefulWidget{
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Spot Center"),centerTitle: true, backgroundColor: Colors.grey,),
-      body: Container(
-        color: Colors.grey.shade100,
-        padding: EdgeInsets.all(30),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          padding: EdgeInsets.all(10),
-          children: <Widget>[
-            SpotCard(),
-            SpotCard(),
-            SpotCard(),
-            SpotCard(),
-            SpotCard()
-          ]
-          ),
-      ),
-      bottomNavigationBar: NavigationBar(
-      backgroundColor: Colors.grey,
-      destinations: const <Widget>[
-        NavigationDestination(
-          icon: Icon(Icons.list), 
-          label: "Spoty"
-          ),
-        NavigationDestination(
-          icon: Icon(Icons.map_outlined),
-          selectedIcon: Icon(Icons.map), 
-          label: "Mapa"
-          ),
-        NavigationDestination(
-          icon: Icon(Icons.person_2_outlined),
-          selectedIcon: Icon(Icons.person_2)
-          , label: "Profil"
-          )
-      ],
-      )
-    );
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("Spot Center"),
+              centerTitle: true,
+              backgroundColor: Colors.grey,
+            ),
+            body: Container(
+              color: Colors.grey.shade100,
+              padding: const EdgeInsets.all(30),
+              child: ListView(
+                  scrollDirection: Axis.vertical,
+                  padding: const EdgeInsets.all(10),
+                  children: const <Widget>[
+                    SpotCard(),
+                    SpotCard(),
+                    SpotCard(),
+                    SpotCard(),
+                    SpotCard()
+                  ]),
+            ),
+            bottomNavigationBar: NavigationBar(
+              backgroundColor: Colors.grey,
+              destinations: const <Widget>[
+                NavigationDestination(icon: Icon(Icons.list), label: "Spoty"),
+                NavigationDestination(
+                    icon: Icon(Icons.map_outlined),
+                    selectedIcon: Icon(Icons.map),
+                    label: "Mapa"),
+                NavigationDestination(
+                    icon: Icon(Icons.person_2_outlined),
+                    selectedIcon: Icon(Icons.person_2),
+                    label: "Profil")
+              ],
+            )));
   }
 }
 
@@ -59,8 +57,8 @@ class SpotCard extends StatelessWidget {
     return Card(
       child: InkWell(
         onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const SpotDescription())
-          );
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const SpotDescription()));
         },
         child: Container(
           width: 350,
