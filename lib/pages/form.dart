32c2bf496b07ab54dd42e7b8ff5e0ba482e2ffc6
llsasas <94858035+llsasas@users.noreply.github.com>
2023-12-12 12:08:30 +0100

    Created class FormWidget

--- lib/pages/form.dart
+++ lib/pages/form.dart
@@ -2,6 +2,8 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:ituapp/pages/storage_handler.dart';
 
+final _formkey = GlobalKey<FormState>();
+
 class FilePickerButton extends StatelessWidget
 {
   @override
@@ -34,5 +36,44 @@ class FilePickerButton extends StatelessWidget
     
   );
 }
+}
 
+class FormWidget extends StatelessWidget
+{
+  const FormWidget({Key? key}) : super (key: key);
+  @override
+  Widget build(BuildContext context)
+  {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formkey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.place),
+                labelText: "Název spotu",
+              ),
+              validator: (value) {
+                if(value == null || value.isEmpty)
+                {
+                  return "Invalid input typed";
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: ()
+              {
+                if(_formkey.currentState!.validate()) {
+
+                }
+              }, child: const Text("Přidat spot")
+              ),
+          ],
+        ),
+       ),
+    );
+  }
 }
