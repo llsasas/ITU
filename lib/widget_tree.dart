54faa1a94aee881060773b105f3f8bdba85b6871
llsasas <94858035+llsasas@users.noreply.github.com>
2023-12-14 11:45:44 +0100

    Create widget_tree.dart

---
+++ lib/widget_tree.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:ituapp/pages/register_page.dart';
+import 'package:ituapp/pages/home.dart';
+import 'package:ituapp/auth.dart';
+
+class WidgetTree extends StatefulWidget
+{
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return StreamBuilder(stream: Auth().authStateChanges,
+     builder: (context, snapshot) {
+       if(snapshot.hasData)
+       {
+        return HomeScreen();
+       }
+       else{
+        return LoginPage();
+       }
+     },);
+  }
+}
