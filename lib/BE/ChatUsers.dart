abe39914cdedaab21b2063af5dd678b28bd3db35
llsasas <94858035+llsasas@users.noreply.github.com>
2023-12-16 16:15:28 +0100

    Update ChatUsers.dart

--- lib/BE/ChatUsers.dart
+++ lib/BE/ChatUsers.dart
@@ -9,13 +9,13 @@ class ChatUsers{
 }
 
   List<ChatUsers> chatUsers = [
-    ChatUsers( name: "Jane Russel",  message: "Awesome Setup",  imagelink: "images/userImage1.jpeg", time: "Now"),
-    ChatUsers( name: "Glady's Murphy",  message: "That's Great",  imagelink: "images/userImage2.jpeg",  time: "Yesterday"),
-    ChatUsers( name: "Jorge Henry",  message: "Hey where are you?",  imagelink: "images/userImage3.jpeg",  time: "31 Mar"),
-    ChatUsers( name: "Philip Fox",  message: "Busy! Call me in 20 mins",  imagelink: "images/userImage4.jpeg", time: "28 Mar"),
-    ChatUsers( name: "Debra Hawkins",  message: "Thankyou, It's awesome",  imagelink: "images/userImage5.jpeg",  time: "23 Mar"),
-    ChatUsers( name: "Jacob Pena",  message: "will update you in evening",  imagelink: "images/userImage6.jpeg", time: "17 Mar"),
-    ChatUsers( name: "Andrey Jones",  message: "Can you please share the file?",  imagelink: "images/userImage7.jpeg", time: "24 Feb"),
-    ChatUsers( name: "John Wick",  message: "How are you?",  imagelink: "images/userImage8.jpeg",  time: "18 Feb"),
+    ChatUsers( name: "Jane Russel",  message: "Awesome Setup",  imagelink: "https://images.generated.photos/FkETUiEtr4GybYlSl6MZEf-epdB-X4F2beuGTXlyQ94/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDMwOTc0LmpwZw.jpg", time: "Now"),
+    ChatUsers( name: "Glady's Murphy",  message: "That's Great",  imagelink: "https://images.generated.photos/G1PTNJf13lWdHeHuJSoTFGlgJzEEx-2Qob3-WNnhZg0/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODQzMzM2LmpwZw.jpg",  time: "Yesterday"),
+    ChatUsers( name: "Jorge Henry",  message: "Hey where are you?",  imagelink: "https://images.generated.photos/rnTuOpM5NrvLh_DWeu11Kv-hPqPeSjuEFcCTvIYsj0g/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/Mzk1NTIzLmpwZw.jpg",  time: "31 Mar"),
+    ChatUsers( name: "Philip Fox",  message: "Busy! Call me in 20 mins",  imagelink: "https://images.generated.photos/tLH5vMqxQ8GtC7QSuB0ezWjQsBeALVu_2gOkCyO54t4/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDM1ODEzLmpwZw.jpg", time: "28 Mar"),
+    ChatUsers( name: "Debra Hawkins",  message: "Thankyou, It's awesome",  imagelink: "https://images.generated.photos/5y6Oph5PrNt22KAtUrd-xZ6D8NuWwjlgfwdv2mDNmt4/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/Nzc5MzczLmpwZw.jpg",  time: "23 Mar"),
+    ChatUsers( name: "Jacob Pena",  message: "will update you in evening",  imagelink: "https://images.generated.photos/CJbzAohKfXboXqlrlAFy4qYtOEqCr7GIauym6CIkEkA/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTc5NjAwLmpwZw.jpg", time: "17 Mar"),
+    ChatUsers( name: "Andrey Jones",  message: "Can you please share the file?",  imagelink: "https://images.generated.photos/tOvHiPeZiwxYVAj2UJze64IQwXZIBa5IstSYs6AvfSY/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NjgzODMwLmpwZw.jpg", time: "24 Feb"),
+    ChatUsers( name: "John Wick",  message: "How are you?",  imagelink: "https://images.generated.photos/Nleo4JiZ0h_KDuh4LboLZOGBtx2cQn86qHNyYpIsVbw/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzM5ODE1LmpwZw.jpg",  time: "18 Feb"),
   ];
 
