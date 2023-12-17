482ced577f2a1a988922e610df1cccf64f160763
llsasas <94858035+llsasas@users.noreply.github.com>
2023-12-17 13:12:54 +0100

    Fix typo mistake

--- lib/BE/spots.dart
+++ lib/BE/spots.dart
@@ -14,7 +14,7 @@ List<Spot> spotslist =
 [
   Spot(name: "Moravská zemská knihovna", address: "Kounicova 65a, 601 87 Brno-střed", description: desc, level: 3, picturelinks: ["Moravská zemská knihovna/6.jpg","Moravská zemská knihovna/8.jpg"]),
   Spot(name: "Hlavas", address: "Benešova 612/22, 602 00 Brno-střed", description: desc, level: 2,picturelinks: ["Hlavas/ds.jpg", "Hlavas/kravihora.jpg", "Hlavas/sokol.jpg", "Hlavas/spot.jpg"]),
-  Spot(name: "Náměstí Míru", address: "nám. Míru 377, 602 00 Brno-střed-Stránice", description: desc, level: 1, picturelinks: ["Náměstí Míru/1.jpg", "Náměstí Míru/2.jpg"]),
+  Spot(name: "Náměstí Míru", address: "nám. Míru 377, 602 00 Brno-střed-Stránice", description: desc, level: 1, picturelinks: ["Naměstí Míru/1.jpg", "Naměstí Míru/2.jpg"]),
   Spot(name: "Orel Řečkovice", address: "Veselka 21, 621 00 Brno-Řečkovice a Mokrá Hora", description: desc, level: 3, picturelinks: ["Orel Řečkovice/3.jpg", "Orel Řečkovice/4.jpg"]),
   Spot(name: "Plachťák", address: "Sportovně rekreační areál Park Pod Plachtami, 637 00 Brno-Nový Lískovec", description: desc, level: 1,picturelinks: ["Plachťák/5.jpeg"]),
   Spot(name: "Chrlice", address: "Blümlova 783/21, 643 00 Brno-Chrlice", description: desc, level: 1, picturelinks: ["Chrlice/colo.jpg", "Chrlice/dsds.jpg", "Chrlica/spota.jpg"]),
