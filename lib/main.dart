107a2f3ad0936002cbc9f0e424dae7af68315068
llsasas <94858035+llsasas@users.noreply.github.com>
2023-12-17 16:49:24 +0100

    Push

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:ituapp/firebase_options.dart';
-import 'package:ituapp/pages/home.dart';
-import 'package:ituapp/auth.dart';
 import 'package:ituapp/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyA4rgQCtgpqt7GZu7o8Ubi7Dy8Rk7SkZ6I",
+          authDomain: "itu-6f4f6.firebaseapp.com",
+          databaseURL:
+              "https://itu-6f4f6-default-rtdb.europe-west1.firebasedatabase.app",
+          projectId: "itu-6f4f6",
+          storageBucket: "itu-6f4f6.appspot.com",
+          messagingSenderId: "483894081462",
+          appId: "1:483894081462:web:618fa7e26b922892ed7317",
+          measurementId: "G-G5XZ384L9C"));
   runApp(const MainApp());
-
 }
 
 class MainApp extends StatelessWidget {
